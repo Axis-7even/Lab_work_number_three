83f2cbcff1a52464ab2d065a658f8c6f9c278461
Axis-7even <yaroslav20021994@gmail.com>
2023-11-16 11:29:32 +0200

    three

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    int count = 0;
+    public void countUp(View view) {
+        count++;
+        TextView counter = findViewById(R.id.show_count);
+        counter.setText(Integer.toString(count));
+    }
+}
